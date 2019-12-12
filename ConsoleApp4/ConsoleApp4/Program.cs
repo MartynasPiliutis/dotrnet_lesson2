fbb5f9cba7e39c5bebb0c467572a147e67911c4d
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-12 19:50:27 +0200

    L2T4 complete

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            string schoolName;
+            string courseName;
+            double studQuant;
+            DateTime dateToday = DateTime.Today;
+            Console.Write("Iveskite mokylos pavadinima: ");
+            schoolName = Console.ReadLine();
+            Console.Write("Iveskite kurso pavadinima: ");
+            courseName = Console.ReadLine();
+            Console.Write("Iveskite studentu kieki: ");
+            studQuant = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Mokyklos pavadinimas: {0}", schoolName);
+            Console.WriteLine("Kurso pavadinimas: {0}", courseName);
+            Console.WriteLine("Studentu kiekis: {0}", studQuant);
+            Console.WriteLine("Siandien yra {0}", dateToday.ToLongDateString());
+            Console.ReadLine();
         }
     }
 }
