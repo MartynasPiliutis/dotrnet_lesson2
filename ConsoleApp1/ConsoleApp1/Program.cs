d5aa8ea7b9675815318e339bef686189eec3669a
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-12 18:45:32 +0200

    l2t1 complete

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int YearOfBirth;
+            string FirstName;
+
+            var LastName = "Pavardenis";
+            decimal Salary = 1999.9m;
+            DateTime LastPayDay = DateTime.Today.AddMonths(-1);
+
+            YearOfBirth = 1990;
+            FirstName = "Vardenis";
+
+            Console.WriteLine("Vardas, Pavarde: {0} {1}", FirstName, LastName);
+            Console.WriteLine("Gimimo metai: {0}", YearOfBirth);
+            Console.WriteLine("Pajamos: {0}", Salary);
+            Console.WriteLine("Paskutini karta ismoketa alga: {0}", LastPayDay.ToLongDateString());
+            Console.ReadLine();
         }
     }
 }
