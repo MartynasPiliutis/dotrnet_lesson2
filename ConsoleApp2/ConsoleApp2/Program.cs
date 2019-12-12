d01695d1b976c616cd89dc5b10cfdbac99ea7a22
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-12 19:20:33 +0200

    L2T2 complete

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,18 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            double num1;
+            double num2;
+
+            Console.Write("Iveskite pirmaji skaiciu: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Iveskite antraji skaiciu: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Skaiciu suma yra: {0}", num1 + num2);
+            Console.WriteLine("Skaiciu atimtis yra: {0}", num1 - num2);
+            Console.WriteLine("Skaiciu daugyba yra: {0}", num1 * num2);
+            Console.WriteLine("Skaiciu dalyba yra: {0}", num1 / num2);
+            Console.ReadLine();
         }
     }
 }
