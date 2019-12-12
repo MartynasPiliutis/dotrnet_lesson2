c0442d74719a8d39684fc926419dda8f01be9107
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2019-12-12 21:15:35 +0200

    L2T6 task1 and task2

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp6
     {
         static void Main(string[] args)
         {
+            double num;
+            Console.Write("Iveskite skaiciu: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} {0} {0} {0}", num);
+            Console.ReadLine();
+
+            double numA=1;
+            double numB=15;
+            double numBuff;
+
+            Console.WriteLine("A = {0} B = {1}", numA, numB);
+
+            numBuff = numA;
+            numA = numB;
+            numB = numBuff;
+
+            Console.WriteLine("A = {0} B = {1}", numA, numB);
+            Console.ReadLine();
         }
     }
 }
